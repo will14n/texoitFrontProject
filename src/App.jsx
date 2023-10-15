5f5c533fd31b604d03367f680c0f31f0618a7593
Willian <willianf4m@gmail.com>
2023-10-15 18:01:16 -0300

    feat: Create a header section and fixed the lateral nav in left

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes, Link } from "react-router-dom"
 import { NavBar } from "./routes/components/NavBar"
 import { MovieScreen } from "./routes/MovieScreen"
 import { DashboardScreen } from "./routes/DashboardScreen"
@@ -7,13 +7,32 @@ import { MovieProvider } from "./routes/context/MovieProvider"
 export const App = () => {
   return (
     <MovieProvider>
-      <NavBar />
-      <Routes>
-        <Route path='/'></Route>
-        <Route path='/dashboard' element={<DashboardScreen />}>Dashboard</Route>
-        <Route path='/movie' element={<MovieScreen />} >Movies</Route>
-        <Route path='/*' element={<Navigate to='/' />}></Route>
-      </Routes>
+      <header data-bs-theme="dark">
+        <div className="navbar navbar-dark bg-dark shadow-sm">
+          <div className="container">
+            <Link to="/" className="navbar-brand d-flex align-items-center">
+              <label>Frontend React Test</label>
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className="container-fluid text-center">
+          <div className="row content ">
+            <div className="col-sm-2 sidenav bg-light">
+              <NavBar />
+            </div>
+            <div className="col-sm-10 text-left">
+              <Routes>
+                <Route path='/'></Route>
+                <Route path='/dashboard' element={<DashboardScreen />}>Dashboard</Route>
+                <Route path='/movie' element={<MovieScreen />} >Movies</Route>
+                <Route path='/*' element={<Navigate to='/' />}></Route>
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </main>
     </MovieProvider>
   )
 }
