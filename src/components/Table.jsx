d97511a16bf1658eb803f3a58b3cad5631447dcb
Willian <willianf4m@gmail.com>
2023-10-15 19:11:58 -0300

    feat: Created component table

---
+++ src/components/Table.jsx
@@ -0,0 +1,30 @@
+export const Table = ({ colls, data, titleAlign = 'text-start', rowsAlign = 'text-start' }) => {
+  return (
+    <>
+      <table className="table table-striped">
+        <thead className={titleAlign}>
+          <tr>
+            {
+              colls.map((line, index) =>
+                <th key={index} scope="col">{line}</th>
+              )
+            }
+          </tr>
+        </thead>
+        <tbody className={"table-group-divider " + rowsAlign}>
+          {
+            data.map((line, index) =>
+              <tr key={index}>
+                {
+                  Object.keys(data[index]).map((item, idx) =>
+                    <td key={idx}>{data[index][item]}</td>
+                  )
+                }
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </>
+  )
+}
