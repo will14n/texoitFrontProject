f87723319a7e0fbc8ad957c908b831c94001a4fe
Willian <willianf4m@gmail.com>
2023-10-16 05:36:46 -0300

    feat: ajusts in provider to operate in all system

--- src/routes/context/MovieProvider.jsx
+++ src/routes/context/MovieProvider.jsx
@@ -1,11 +1,31 @@
-import React, { Children } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MovieContext } from './MovieContext'
 
-const movies = { "content": [{ "id": 1, "year": 1980, "title": "Can't Stop the Music", "studios": ["Associated Film Distribution"], "producers": ["Allan Carr"], "winner": true }, { "id": 2, "year": 1980, "title": "Cruising", "studios": ["Lorimar Productions", "United Artists"], "producers": ["Jerry Weintraub"], "winner": false }, { "id": 3, "year": 1980, "title": "The Formula", "studios": ["MGM", "United Artists"], "producers": ["Steve Shagan"], "winner": false }, { "id": 4, "year": 1980, "title": "Friday the 13th", "studios": ["Paramount Pictures"], "producers": ["Sean S. Cunningham"], "winner": false }, { "id": 5, "year": 1980, "title": "The Nude Bomb", "studios": ["Universal Studios"], "producers": ["Jennings Lang"], "winner": false }, { "id": 6, "year": 1980, "title": "The Jazz Singer", "studios": ["Associated Film Distribution"], "producers": ["Jerry Leider"], "winner": false }, { "id": 7, "year": 1980, "title": "Raise the Titanic", "studios": ["Associated Film Distribution"], "producers": ["William Frye"], "winner": false }, { "id": 8, "year": 1980, "title": "Saturn 3", "studios": ["Associated Film Distribution"], "producers": ["Stanley Donen"], "winner": false }, { "id": 9, "year": 1980, "title": "Windows", "studios": ["United Artists"], "producers": ["Mike Lobell"], "winner": false }, { "id": 10, "year": 1980, "title": "Xanadu", "studios": ["Universal Studios"], "producers": ["Lawrence Gordon"], "winner": false }, { "id": 11, "year": 1981, "title": "Mommie Dearest", "studios": ["Paramount Pictures"], "producers": ["Frank Yablans"], "winner": true }, { "id": 12, "year": 1981, "title": "Endless Love", "studios": ["PolyGram", "Universal Studios"], "producers": ["Dyson Lovell"], "winner": false }, { "id": 13, "year": 1981, "title": "Heaven's Gate", "studios": ["United Artists"], "producers": ["Joann Carelli"], "winner": false }, { "id": 14, "year": 1981, "title": "The Legend of the Lone Ranger", "studios": ["Associated Film Distribution", "Universal Studios"], "producers": ["Walter Coblenz"], "winner": false }, { "id": 15, "year": 1981, "title": "Tarzan, the Ape Man", "studios": ["MGM", "United Artists"], "producers": ["John Derek"], "winner": false }], "pageable": { "sort": { "unsorted": true, "sorted": false, "empty": true }, "offset": 0, "pageSize": 99, "pageNumber": 0, "unpaged": false, "paged": true }, "last": false, "totalPages": 3, "totalElements": 206, "size": 99, "number": 0, "sort": { "unsorted": true, "sorted": false, "empty": true }, "first": true, "numberOfElements": 99, "empty": false }
-
 export const MovieProvider = ({ children }) => {
+
+
+  const [movies, setMovies] = useState()
+  const [yearsWithMultipleWinners, setYearsWithMultipleWinners] = useState()
+  const [studiosWithWinCountTop3, setStudiosWithWinCountTop3] = useState()
+  const [maxMinWinIntervalForProducers, setMaxMinWinIntervalForProducers] = useState()
+  const [listMovieWinnersByYear, setListMovieWinnersByYear] = useState()
+  const [yearFilter, setYearFilter] = useState()
+
   return (
-    <MovieContext.Provider value={{ movies }}>
+    <MovieContext.Provider value={{
+      movies,
+      yearsWithMultipleWinners,
+      studiosWithWinCountTop3,
+      maxMinWinIntervalForProducers,
+      listMovieWinnersByYear,
+      yearFilter,
+      setMovies,
+      setYearsWithMultipleWinners,
+      setStudiosWithWinCountTop3,
+      setMaxMinWinIntervalForProducers,
+      setListMovieWinnersByYear,
+      setYearFilter,
+    }}>
       {children}
     </MovieContext.Provider>
   )
