01d5d5248395f62cf2b13d70a970591cce3a4ffa
Willian <willianf4m@gmail.com>
2023-10-16 11:42:37 -0300

    feat: Make the pagination work with the api

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,36 +1,65 @@
 import { Link } from "react-router-dom"
 
-export const Pagination = ({totalPages, page}) => {
+export const Pagination = ({ totalPages, page, setPageLink }) => {
   const previousPage = page === 0 ? 0 : page - 1
   const nextPage = page === (totalPages - 1) ? page : page + 1
   const currentPage = page
   const lastPage = totalPages - 1
+
   return (
     <>
       <nav aria-label="Page navigation">
         <ul className="pagination justify-content-center">
           <li className="page-item">
-            <Link to="?page=0" className={"page-link " + ((page === 0) ? 'disabled' : '')} aria-label="Previous">
+            <Link
+              onClick={() => setPageLink('page=0')}
+              to="?page=0"
+              className={"page-link " + ((page === 0) ? 'disabled' : '')}
+              aria-label="Previous"
+            >
               <span aria-hidden="true">First</span>
             </Link>
           </li>
           <li className="page-item">
-            <Link to={"?page=" + previousPage} className={"page-link " + ((page === 0) ? 'disabled' : '')} aria-label="Previous">
+            <Link
+              onClick={() => setPageLink('page=0' + previousPage)}
+              to={"?page=" + previousPage}
+              className={"page-link " + ((page === 0) ? 'disabled' : '')}
+              aria-label="Previous"
+            >
               <span aria-hidden="true">&laquo;</span>
             </Link>
           </li>
           {
             [...Array(totalPages)].map(
-              (e, i) => <li className="page-item" key={i}><Link  to={"?page=" + i} className={"page-link " + ((page === i) ? 'active disable' : '')}>{i + 1}</Link></li>
+              (e, i) => <li className="page-item" key={i}>
+                {console.log(page)}
+                <Link
+                  onClick={() => setPageLink('page=' + (i))}
+                  to={"?page=" + i}
+                  className={"page-link " + ((page === i) ? 'active disable' : '')}
+                >
+                  {i + 1}
+                </Link>
+                </li>
             )
           }
           <li className="page-item">
-            <Link to={"?page=" + nextPage} className={"page-link " + ((page === (totalPages - 1)) ? 'disabled' : '')} aria-label="Next">
+            <Link
+              onClick={() => setPageLink('page=' + (nextPage))}
+              to={"?page=" + nextPage}
+              className={"page-link " + ((page === (totalPages - 1)) ? 'disabled' : '')}
+              aria-label="Next"
+              >
               <span aria-hidden="true">&raquo;</span>
             </Link>
           </li>
           <li className="page-item">
-            <Link to={"?page=" + lastPage} className={"page-link " + ((page === (totalPages - 1)) ? 'disabled' : '')} aria-label="Next">
+            <Link
+              onClick={() => setPageLink('page=' + (totalPages - 1))} 
+              to={"?page=" + lastPage} className={"page-link " + ((page === (totalPages - 1)) ? 'disabled' : '')}
+              aria-label="Next"
+            >
               <span aria-hidden="true">Last</span>
             </Link>
           </li>
