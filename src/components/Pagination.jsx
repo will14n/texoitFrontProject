6eb6db080ec7b9de05579044e560dc0dad80dec4
Willian <willianf4m@gmail.com>
2023-11-05 04:33:18 -0300

    refactor: Ajusts un pagination to work in movieScreen and reactored the movieScreen component

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,24 +1,24 @@
 import { Link } from "react-router-dom"
 
 export const Pagination = ({ totalPages, page, setPageLink }) => {
-  const previousPage = page === 0 ? 0 : page - 1
-  const nextPage = page === (totalPages - 1) ? page : page + 1
   const currentPage = page
+  const previousPage = currentPage === 0 ? 0 : currentPage - 1
+  const nextPage = currentPage === (totalPages - 1) ? currentPage : currentPage + 1
   const lastPage = totalPages - 1
 
   return (
     <>
-      <nav aria-label="Page navigation" className="bg-light">
+      <nav aria-label="Page navigation" className="border bg-light">
         <ul className="pagination justify-content-center align-text-bottom mb-0">
           <li className="page-item">
             <Link
               onClick={() => setPageLink('page=0')}
               to="?page=0"
-              className={"d-md-none d-lg-block border-0 bg-light text-dark page-link " + ((page === 0) ? 'disabled' : '')}
-              aria-label="Previous"
+              className={"d-md-none d-lg-block border-0 bg-light text-dark page-link " + ((currentPage === 0) ? 'disabled' : '')}
+              aria-label="First"
             >
               <span aria-hidden="true">
-                <svg className={page === 0 ? 'gray-200' : ''} id="i-start" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="12" height="12" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3">
+                <svg className={currentPage === 0 ? 'gray-200' : ''} id="i-start" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="12" height="12" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3">
                   <path d="M8 2 L8 16 22 2 22 30 8 16 8 30" />
                 </svg>
               </span>
@@ -28,11 +28,11 @@ export const Pagination = ({ totalPages, page, setPageLink }) => {
             <Link
               onClick={() => setPageLink('page=0' + previousPage)}
               to={"?page=" + previousPage}
-              className={"border-0 bg-light text-dark page-link " + ((page === 0) ? ' disabled' : '')}
+              className={"border-0 bg-light text-dark page-link " + ((currentPage === 0) ? ' disabled' : '')}
               aria-label="Previous"
             >
               <span aria-hidden="true">
-                <svg className={page === 0 ? 'text-gray-200' : ''} id="i-caret-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="12" height="12" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3">
+                <svg className={currentPage === 0 ? 'text-gray-200' : ''} id="i-caret-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="12" height="12" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3">
                   <path d="M22 30 L6 16 22 2 Z" />
                 </svg>
               </span>
@@ -44,7 +44,7 @@ export const Pagination = ({ totalPages, page, setPageLink }) => {
                 <Link
                   onClick={() => setPageLink('page=' + (i))}
                   to={"?page=" + i}
-                  className={"border-0  text-dark page-link " + ((page === i) ? 'disable bg-primary' : 'bg-light')}
+                  className={"border-0  text-dark page-link " + ((currentPage === i) ? 'disable bg-primary' : 'bg-light')}
                 >
                   {i + 1}
                 </Link>
@@ -55,7 +55,7 @@ export const Pagination = ({ totalPages, page, setPageLink }) => {
             <Link
               onClick={() => setPageLink('page=' + (nextPage))}
               to={"?page=" + nextPage}
-              className={"border-0 bg-light text-dark page-link " + ((page === (totalPages - 1)) ? 'disabled' : '')}
+              className={"border-0 bg-light text-dark page-link " + ((currentPage === (lastPage)) ? 'disabled' : '')}
               aria-label="Next"
             >
               <span aria-hidden="true">
@@ -67,9 +67,9 @@ export const Pagination = ({ totalPages, page, setPageLink }) => {
           </li>
           <li className="page-item">
             <Link
-              onClick={() => setPageLink('page=' + (totalPages - 1))}
-              to={"?page=" + lastPage} className={"d-md-none d-lg-block border-0 bg-light text-dark page-link " + ((page === (totalPages - 1)) ? 'disabled' : '')}
-              aria-label="Next"
+              onClick={() => setPageLink('page=' + (lastPage))}
+              to={"?page=" + lastPage} className={"d-md-none d-lg-block border-0 bg-light text-dark page-link " + ((currentPage === (lastPage)) ? 'disabled' : '')}
+              aria-label="Last"
             >
               <span aria-hidden="true">
                 <svg id="i-end" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="12" height="12" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3">
--- src/routes/MovieScreen.jsx
+++ src/routes/MovieScreen.jsx
@@ -4,6 +4,7 @@ import { Pagination } from "../components/Pagination"
 import { Card } from "../components/Card"
 import { fetchData } from "../helpers/fetchData"
 import { useSearchParams } from 'react-router-dom';
+import Table from "../components/Table"
 
 export const MovieScreen = () => {
   const {
@@ -20,18 +21,6 @@ export const MovieScreen = () => {
     setMovies(response)
   }
 
-  const handleYearChange = (year) => {
-    setMovieYearFilter(year)
-    if ((year > 1979 && year < 2020) || year === '') {
-      fetchMovies('page=0', winnerFilter, year)
-    }
-  }
-  const handleWinnerChange = (winner) => {
-    setWinnerFilter(winner)
-    // if((year > 1979 && year < 2020) || year === '') {
-    fetchMovies('page=0', winner, movieYearFilter)
-    // }
-  }
   useEffect((e) => {
     fetchMovies(pageLink, winnerFilter, movieYearFilter)
   }, [pageLink])
@@ -39,63 +28,20 @@ export const MovieScreen = () => {
     <>
       <section className="mt-2 p-3">
         <Card title='List Movies'>
-          <table className="table mb-0 bg-light table-sm text-size-5">
-            <thead className="text-center bg-light">
-              <tr className="bg-light col-sm-12">
-                <th className="">ID</th>
-                <th scope="col" >
-                  <div className="">
-                    Year
-                  </div>
-                  <input
-                    type="text"
-                    className="form-control form-control-sm w-100px"
-                    name="movieYearFilter"
-                    value={movieYearFilter || ''}
-                    min="0"
-                    onChange={(e) => handleYearChange(e.target.value)}
-                    placeholder="Filter by year"
-                  />
-                </th>
-                <th scope="col">Title</th>
-                <th scope="col">
-                  <div>
-                    Winner?
-                  </div>
-                  <select className="form-select form-select-sm w-100px" name="winnerFilter" onChange={(e) => handleWinnerChange(e.target.value)}>
-                    <option value=''>Yes/No</option>
-                    <option value="true">Yes</option>
-                    <option value="false">No</option>
-                  </select>
-                </th>
-              </tr>
-            </thead>
-            <tbody className="table-group-divider text-start">
-              {
-                movies &&
-                movies.content.map((movie) =>
-                  <tr key={movie.id}>
-                    <td>{movie.id}</td>
-                    <td>{movie.year}</td>
-                    <td>{movie.title}</td>
-                    <td>{movie.winner ? "Yes" : "No"}</td>
-                  </tr>
-                )
-              }
-              <tr>
-                <td colSpan={4} className="p-0">
-                  {
-                    movies &&
-                    <Pagination
-                      setPageLink={setPageLink}
-                      totalPages={movies.totalPages}
-                      page={movies.number}
-                    />
-                  }
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <Table
+            colls={['ID', 'Year', 'Title', 'Winner?']}
+            data={movies?.content ? movies.content : []}
+            dataConfirm={['id', 'year', 'title', 'winner']}
+            tableSize="table-sm text-size-5"
+            titleAlign='text-center'
+            renderInput={['Year', 'Winner?']}
+            fetchData={fetchMovies}
+          />
+          <Pagination
+            setPageLink={setPageLink}
+            totalPages={movies?.totalPages}
+            page={movies?.number}
+          />
         </Card>
       </section>
     </>
